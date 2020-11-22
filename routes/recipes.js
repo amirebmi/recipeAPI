27f201da6fe2763cd4a81c3fbd9ccfacf038c5b5
amirebmi <irit.network@gmail.com>
2020-11-22 11:48:35 -0800

    Fix minor issues

--- routes/recipes.js
+++ routes/recipes.js
@@ -14,11 +14,11 @@ router.post('/search1', async function(req, res, next) {
 
     let recipes = await req.app.locals.db.collection("recipes");
 
-    let textSearch = req.body.search;
+    let textSearch = req.body.recipe;
 
-    const cursor2 = await recipes.find({$text: {$search: textSearch}}, {$caseSensitive: true});
+    const cursor = await recipes.find({$text: {$search: textSearch}}, {$caseSensitive: true});
 
-    res.json(await cursor2.toArray());
+    res.json(await cursor.toArray());
 });
 
 // Search recipes that use certain ingredients (for example "beef" and "potato").
@@ -26,33 +26,40 @@ router.post('/search2', async function(req, res, next) {
 
     let recipes = await req.app.locals.db.collection("recipes");
 
-    let textSearch1 = req.body.search1;
-    let textSearch2 = req.body.search2;
+    // Get the complete parameters from the body
+    let bodyParamObject = req.body;
 
-    const query = {
-        $and: [{
-        ingredients: { $regex: textSearch1 }
-        },
-        {
-        ingredients: { $regex: textSearch2 }
-        }
-        ]};
+    // Create new arrays
+    const parameters = [];
 
-    const cursor1 = await recipes.find(query, {_id:1,name:1});
+    for (let item in bodyParamObject){
+        parameters.push({ingredients:{ $regex: bodyParamObject[item] }})
+    }
 
-    res.json(await cursor1.toArray());
+    const crsr = await recipes.find({$and:parameters}, {_id:1,name:1});
+
+    res.json(await crsr.toArray());
 });
 
 
 // Add a recipe
-router.post('/', async function(req, res, next){
+router.post('/Add', async function(req, res, next){
 
     let recipes = await req.app.locals.db.collection("recipes");
 
-    let name = req.body.name;
-    let yield = req.body.yield;
-    let ingredients = req.body.ingredients;
-    let directions = req.body.directions;
+    // Create a recipe object
+    const recipe = {
+        name: req.body.name,
+        yield: req.body.yield,
+        ingredients: req.body.ingredients,
+        directions: req.body.directions
+    }
+
+    // Add the recipe object to the database
+    await recipes.insertOne(recipe);
+
+    // response Create status code and the object's Id
+    res.status(201).send(recipe._id);
 });
 
 
